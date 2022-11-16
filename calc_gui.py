777670962285482af912b86ce814abc65482443c
bobbyboy2007 <103608477+bobbyboy2007@users.noreply.github.com>
2022-11-17 09:57:39 +1300

    Add files via upload

---
+++ calc_gui.py
@@ -0,0 +1,62 @@
+#A S Sandhu
+
+#...................................................
+#Python Calculator with GUI (graphic user interface)
+#...................................................
+
+
+from tkinter import * #import the tkinter library
+def button_press(num):
+    pass
+
+def equals()
+    pass
+
+def clear():
+    pass
+
+window = Tk()
+window.title("Calculator Program")
+window.geometry(500x500p)
+window.configure(bg="light grey")
+
+equation_text = ""
+
+equarion_label = stringvar()
+
+Label = Label(window, textvariable=equation_label, font=('console', 20), gb="white", width=24, hight=1)
+label.pack()
+
+button1 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(1))
+button1.grid(row=2, colum=2)
+
+button2 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(2))
+button2.grid(row=2, colum=2)
+
+button3 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(3))
+button3.grid(row=2, colum=2)
+
+button4 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(4))
+button4.grid(row=2, colum=2)
+
+button5 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(5))
+button5.grid(row=2, colum=2)
+
+button6 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(6))
+button6.grid(row=2, colum=2)
+
+button7 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(7))
+button7.grid(row=2, colum=2)
+
+button8 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(8))
+button8.grid(row=2, colum=2)
+
+button9 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(9))
+button9.grid(row=2, colum=2)
+
+button0 = Button(frame, text=8, hieght=4, width=9, font=35, command=lambda: buttom_press(0))
+button0.grid(row=2, colum=2)
+
+
+
+
